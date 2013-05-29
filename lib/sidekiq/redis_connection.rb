ada6b0e73fa3c197537bba41f0051a1be2345cb0
Peter Bui <peter@paydrotalks.com>
2013-05-28 17:55:46 -0700

    Really get redis + sentinel to work

--- lib/sidekiq/redis_connection.rb
+++ lib/sidekiq/redis_connection.rb
@@ -4,21 +4,22 @@ require 'redis'
 module Sidekiq
   class RedisConnection
     def self.create(options={})
-      url = options[:url] || determine_redis_provider || 'redis://localhost:6379/0'
+      options[:url] = options[:url] || determine_redis_provider || 'redis://localhost:6379/0' unless options[:sentinel]
+
       # need a connection for Fetcher and Retry
       size = options[:size] || (Sidekiq.server? ? (Sidekiq.options[:concurrency] + 2) : 5)
 
       ConnectionPool.new(:timeout => 1, :size => size) do
-        build_client(url, options[:namespace], options[:driver] || 'ruby', options[:sentinels])
+        if options[:sentinels] && options[:sentinels].length > 0
+          build_sentinel_client(options)
+        else
+          build_client(options[:url], options[:namespace], options[:driver] || 'ruby')
+        end
       end
     end
 
-    def self.build_client(url, namespace, driver, sentinels)
-      if sentinels && sentinels.length > 0
-        client = Redis.new(:master_name => url, :sentinels => sentinels, :driver => driver)
-      else
-        client = Redis.connect(:url => url, :driver => driver)
-      end
+    def self.build_client(url, namespace, driver)
+      client = Redis.connect(:url => url, :driver => driver)
 
       if namespace
         require 'redis/namespace'
@@ -28,6 +29,18 @@ module Sidekiq
       end
     end
     private_class_method :build_client
+
+    def self.build_sentinel_client(options)
+      client = Redis.new(options.dup)
+
+      if options[:namespace]
+        require 'redis/namespace'
+        Redis::Namespace.new(options[:namespace], :redis => client)
+      else
+        client
+      end
+    end
+    private_class_method :build_sentinel_client
 
     # Not public
     def self.determine_redis_provider
