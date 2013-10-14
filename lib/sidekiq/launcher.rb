4299523ccf5588a2a96d82fb16dab7f2c467c079
Peter Bui <peter@paydrotalks.com>
2013-10-14 09:57:59 -0700

    Add statsd pinger thread
    Depends on a STATSD constant.

--- lib/sidekiq/launcher.rb
+++ lib/sidekiq/launcher.rb
@@ -3,24 +3,31 @@ require 'sidekiq/manager'
 require 'sidekiq/scheduled'
 require 'sidekiq/digest_poller'
 
+PING = defined?(STATSD)
+require 'sidekiq/pinger' if PING
+
 module Sidekiq
   class Launcher
-    attr_reader :manager, :poller, :digest_poller, :options
+    attr_reader :manager, :poller, :digest_poller, :pinger, :options
     def initialize(options)
-      @options = options
-      @manager = Sidekiq::Manager.new(options)
-      @poller  = Sidekiq::Scheduled::Poller.new
+      @options       = options
+      @manager       = Sidekiq::Manager.new(options)
+      @poller        = Sidekiq::Scheduled::Poller.new
       @digest_poller = Sidekiq::Scheduled::DigestPoller.new
+      @pinger        = Sidekiq::Scheduled::Pinger.new if PING
     end
 
     def run
       manager.async.start
       poller.async.poll(true)
       digest_poller.async.poll(true)
+      pinger.async.poll(true) if pinger
     end
 
     def stop
       poller.async.terminate if poller.alive?
+      digest_poller.async.terminate if digest_poller.alive?
+      pinger.async.terminate if pinger && pinger.alive?
       manager.async.stop(:shutdown => true, :timeout => options[:timeout])
       manager.wait(:shutdown)
     end
---
+++ lib/sidekiq/pinger.rb
@@ -0,0 +1,31 @@
+require 'sidekiq'
+require 'sidekiq/util'
+require 'celluloid'
+
+module Sidekiq
+  module Scheduled
+    class Pinger
+      include Celluloid
+      include Sidekiq::Util
+
+      PING_INTERVAL = 60
+
+      def poll(first_time=false)
+        watchdog('scheduling digest poller thread died!') do
+          add_jitter if first_time
+          STATSD.count("sidekiq.processor")
+          after(PING_INTERVAL) { poll }
+        end
+      end
+
+      def add_jitter
+        begin
+          sleep(PING_INTERVAL * rand)
+        rescue Celluloid::Task::TerminatedError
+          # Hit Ctrl-C when Sidekiq is finished booting and we have a chance
+          # to get here.
+        end
+      end
+    end
+  end
+end
